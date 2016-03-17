304b248995eb14f5ef9792b4668f753b217dc94c
weakiwi <670932228@qq.com>
2016-03-17 19:17:16 +0800

    Update index.php
    细化mysql，下一步是增加id还有hot_degree

--- index.php
+++ index.php
@@ -9,9 +9,9 @@ libxml_use_internal_errors(true);
 $wechatObj = new wechatCallbackapiTest();
 header("content-Type: text/html; charset=gbk");
 $servername = "localhost";
-$username = " ";
-$password = " ";
-$dbname = "weixinLibrary";
+$username = "";
+$password = "";
+$dbname = "";
 
 $conn = mysql_connect($servername, $username, $password);//创建sql连接
 if(! $conn )
@@ -78,6 +78,43 @@ class wechatCallbackapiTest
             $data = htmlspecialchars($data);
             return $data;     
         }     
+        function curl_get_contents($url,$timeout=5,$method='get',$post_fields=array(),$reRequest=3,$referer="") { //封装 curl
+               $ch = curl_init();
+               curl_setopt($ch, CURLOPT_URL, $url);
+               curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+               curl_setopt($ch, CURLOPT_HEADER, 0);
+               curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
+               curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE );
+               curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false );
+               curl_setopt($ch, CURLOPT_REFERER, $referer);
+               if (strpos($method,'post')>-1) {
+                   curl_setopt($ch, CURLOPT_POST, true);
+                   curl_setopt($ch, CURLOPT_POSTFIELDS,$post_fields);
+               }
+               if (strpos($method,'WithHeader')>-1) {
+                   curl_setopt($ch, CURLOPT_HEADER, true);
+                   curl_setopt($ch, CURLOPT_NOBODY, false);
+               }
+               $output = curl_exec($ch);
+               if (curl_errno($ch)==0) {
+                   if (strpos($method,'WithHeader')>-1) {
+                       $headerSize = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+                       $header = substr($output, 0, $headerSize);
+                       $body = substr($output, $headerSize);
+                       return array($header,$body,$output);
+                   } else {
+                       return $output;
+                   }
+               } else {
+                   if ($reRequest) {
+                       $reRequest--;
+                       return curl_get_contents($url,$timeout,$method,$post_fields,$reRequest);
+                   } else {
+                       return false;
+                   }
+               }
+           }
+        
         if (!empty($postStr)){
             $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);//初始化处理用户发过来的数据
             $fromUsername = $postObj->FromUserName;
@@ -112,8 +149,8 @@ class wechatCallbackapiTest
                 <FuncFlag>0</FuncFlag>     
             </xml>
             ";           
-            $findbook = 'SELECT * FROM books WHERE bookName="%s"';
-            $createbook ='INSERT INTO books (isbn, bookName, detail) VALUES ("%s", "%s","%s")'; 
+            $findbook = 'SELECT * FROM books WHERE book_name="%s"';
+            $createbook ='INSERT INTO books (book_isbn, book_author, book_publisher, book_location, book_states, book_ztflh, book_name) VALUES ("%s", "%s","%s","%s","%s","%s","%s")'; 
             if($event == "subscribe") {
                 $msgType = "text";#回复数据类型为文本
                 $contentStr = "使用方法：book+书籍全名\n如：'book+三国演义'";//回复字串内容为年月日
@@ -140,19 +177,25 @@ class wechatCallbackapiTest
                 $sql = sprintf($findbook, $search);
                 $findbook = mysql_query($sql);
                 if ($row = mysql_fetch_array($findbook)){
-                    $resultStr = sprintf($bilibili_xml, $fromUsername, $toUsername, $time,  $row['bookName'], $row['detail'] . "\n" . $row['isbn'] . "\n", "", "http://115.28.1.228/a.php?isbn=" );
+                    $resultStr = sprintf($bilibili_xml, $fromUsername, $toUsername, $time,  $row['book_name'] . "\n",
+                                                                                            $row['book_author'] . "\n" 
+                                                                                          . $row['book_states'] . "\n"
+                                                                                          . $row['book_location'] . "\n"
+                                                                                          . $row['book_states'] . "\n"
+                                                                                          . $row['book_ztflh']
+                                                                                          . $row['book_publisher'] , "", "http://115.28.1.228/a.php?isbn=" );
                     echo $resultStr;
                 }
                 else {
                     $url = "http://202.118.84.130:1701/primo_library/libweb/action/search.do?ct=facet&fctN=facet_tlevel&fctV=available&rfnGrp=show_only&dscnt=0&frbg=&scp.scps=scope%3A(DLMH)%2Cprimo_central_multiple_fe&tab=default_tab&dstmp=1456801597218&srt=rank&ct=search&mode=Basic&&dum=true&indx=1&vl(freeText0)=".$search;
                     $url = $url."&fn=search&vid=dlmh";//海事大学图书馆书名查找入口
-                    $html_source = file_get_contents($url);
+                    $html_source = curl_get_contents($url);
                 //    echo dom_parser($html_source, "//li[@id='exlidResult0-LocationsTab']/a/@href");//获取图书所在位置
                     $url = "http://202.118.84.130:1701/primo_library/libweb/action/" . dom_parser($html_source, "//li[@id='exlidResult0-LocationsTab']/a/@href");//查找图书所在位置(在架状态页面)
                     $url_2 = "http://202.118.84.130:1701/primo_library/libweb/action/" . dom_parser($html_source, "//a[@id='exlidResult0-detailsTabLink']/@href");//查找图书所在位置(详细信息页面)
-                    $html_source_2 = file_get_contents($url_2);
+                    $html_source_2 = curl_get_contents($url_2);
                     $book_isbn = dom_parser($html_source_2, "//ul/li[@id='识别符-1']/span[@class='EXLDetailsDisplayVal']");
-                    $html_source = file_get_contents($url);
+                    $html_source = curl_get_contents($url);
                     $book_name = dom_parser($html_source, "//h1[@class='EXLResultTitle']");//获取书籍名称
                     $book_author = dom_parser($html_source_2,"//ul/li[@id='著者-1']/a[@class='EXLLinkedField']");//获取书籍作者
                     $book_publisher = dom_parser($html_source_2,"//ul/li[@id='出版发行-1']/span[@class='EXLDetailsDisplayVal']");//获取书籍出版社
@@ -168,7 +211,7 @@ class wechatCallbackapiTest
                     if (ctype_space($contentStr)){
                         $contentStr = "没有你想要找的书籍";
                     }
-                    $createbook = sprintf($createbook, $book_isbn, $book_name, $contentStr);
+                    $createbook = sprintf($createbook, $book_isbn, $book_author, $book_publisher, $book_location, $book_states, $book_ztflh, $book_name);
                     mysql_query($createbook);                  
                     $resultStr = sprintf($bilibili_xml, $fromUsername, $toUsername, $time,  $book_name, $contentStr, "", "http://115.28.1.228/a.php?isbn=" );
                     echo $resultStr;//
