dbd6919a381ad9d73494069afa78686a2ca25e20
weakiwi <670932228@qq.com>
2016-03-18 13:00:07 +0800

    Update spider.py
    增加数据库连接的函数，下一步就是用队列线程多进程把他们串起来了

--- spider.py
+++ spider.py
@@ -2,6 +2,28 @@
 import urllib
 import urllib2
 from lxml import etree
+import Queue
+import MySQLdb
+
+q = Queue.Queue()
+
+def SQLwrite(book_isbn, book_author, book_publisher, book_location, book_state, book_ztfh, book_name):
+    db = MySQLdb.connect("localhost","root"," "," ", " " )
+    cursor = db.cursor()
+    sql = """
+    INSERT INTO books(book_isbn, 
+                      book_author, book_publisher,
+                      book_location, book_state,
+                      book_ztfh, book_name)
+    VALUES ("%s", "%s", "%s", "%s", "%s", "%s", "%s")
+    """ % (book_isbn, book_author, book_publisher, book_location, book_state, book_ztfh, book_name)
+    try:
+        cursor.execute(sql)
+        db.commit()
+    except:
+        db.rollback()
+        
+    db.close()
 
 def spider(num):
     url = 'http://202.118.84.130:1701/primo_library/libweb/action/display.do?tabs=detailsTab&ct=display&fn=search&doc=dmuzw000'+str(num)
@@ -9,13 +31,22 @@ def spider(num):
     page = response.read()
     return page
 
-def analyse(page):
+def analyse_index(page):
     doc = etree.HTML(page.decode('utf-8','ignore'))
     book_name = doc.xpath(u"//div[@class='EXLLinkedFieldTitle']/text()")
-    book_publish = doc.xpath(u"//ul/li[@id='出版发行-1']/span[@class='EXLDetailsDisplayVal']/text()")
+    book_publisher = doc.xpath(u"//ul/li[@id='出版发行-1']/span[@class='EXLDetailsDisplayVal']/text()")
     book_author = doc.xpath(u"//ul/li[@id='著者-1']/a[@class='EXLLinkedField']/text()")
-    return book_name[0]
-
+    book_href = doc.xpath(u"//li[@id='exlidResult0-LocationsTab']/a/@href")
+    location_href = 'http://202.118.84.130:1701/primo_library/libweb/action/' + book_href[]
+    q.put(location_href)
+    return (book_name[0].encode('utf-8'), book_publisher[0].encode('utf-8'), book_author[0].encode('utf-8'))
+    
+def analyse_location(page):
+    doc = etree.HTML(page.decode('utf-8','ignore'))
+    book_ztfh = doc.xpath(u"//cite/text()")
+    book_location = doc.xpath(u"//span[@class='EXLLocationsTitleContainer']/text()")
+    book_state = doc.xpath(u"//td[@class='EXLLocationTableColumn3']/text()")
+    return (book_ztfh[0].encode('utf-8'), book_location[0].encode('utf-8'), book_state[0].encode('utf-8'))
 def main():
     print analyse(spider(100000)).encode('utf-8')
 
