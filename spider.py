05bc9567cbd450d03880b79bc30a0ad86b89aad2
weakiwi <670932228@qq.com>
2016-03-18 14:47:33 +0800

    Update spider.py
    队列写好了，模块也检查好了，下一步是多线程啊

--- spider.py
+++ spider.py
@@ -5,16 +5,19 @@ from lxml import etree
 import Queue
 import MySQLdb
 
-q = Queue.Queue()
+q1 = Queue.Queue()
+q2 = Queue.Queue(maxsize = 7)
+temp_list = []
 
-def SQLwrite(book_isbn, book_author, book_publisher, book_location, book_state, book_ztfh, book_name):
-    db = MySQLdb.connect("localhost","root"," "," ", " " )
+
+def SQLwrite(book_isbn, book_author, book_publisher, book_name, book_location, book_state, book_ztfh):
+    db = MySQLdb.connect("localhost", " ", " ", " " )
     cursor = db.cursor()
     sql = """
     INSERT INTO books(book_isbn, 
                       book_author, book_publisher,
-                      book_location, book_state,
-                      book_ztfh, book_name)
+                      book_name, book_location, book_state,
+                      book_ztfh )
     VALUES ("%s", "%s", "%s", "%s", "%s", "%s", "%s")
     """ % (book_isbn, book_author, book_publisher, book_location, book_state, book_ztfh, book_name)
     try:
@@ -25,30 +28,50 @@ def SQLwrite(book_isbn, book_author, book_publisher, book_location, book_state, 
         
     db.close()
 
-def spider(num):
+def xpathNull(doc, yourPath):
+    temp = doc.xpath(yourPath)
+    if len(temp) == 0:
+        temp.append("NULL")
+        return temp
+    else:
+        return temp
+def analyse_index(num):
     url = 'http://202.118.84.130:1701/primo_library/libweb/action/display.do?tabs=detailsTab&ct=display&fn=search&doc=dmuzw000'+str(num)
     response = urllib.urlopen(url)
     page = response.read()
-    return page
-
-def analyse_index(page):
     doc = etree.HTML(page.decode('utf-8','ignore'))
-    book_name = doc.xpath(u"//div[@class='EXLLinkedFieldTitle']/text()")
-    book_publisher = doc.xpath(u"//ul/li[@id='出版发行-1']/span[@class='EXLDetailsDisplayVal']/text()")
-    book_author = doc.xpath(u"//ul/li[@id='著者-1']/a[@class='EXLLinkedField']/text()")
-    book_href = doc.xpath(u"//li[@id='exlidResult0-LocationsTab']/a/@href")
-    location_href = 'http://202.118.84.130:1701/primo_library/libweb/action/' + book_href[]
-    q.put(location_href)
-    return (book_name[0].encode('utf-8'), book_publisher[0].encode('utf-8'), book_author[0].encode('utf-8'))
+    book_name = xpathNull(doc,u"//div[@class='EXLLinkedFieldTitle']/text()")
+    book_publisher = xpathNull(doc,u"//ul/li[@id='出版发行-1']/span[@class='EXLDetailsDisplayVal']/text()")
+    book_author = xpathNull(doc,u"//ul/li[@id='著者-1']/a[@class='EXLLinkedField']/text()")
+    book_href = xpathNull(doc,u"//li[@id='exlidResult0-LocationsTab']/a/@href")
+    book_isbn = xpathNull(doc,u"//li[@id='识别符-1']/span[@class='EXLDetailsDisplayVal']/text()")
+    location_href = 'http://202.118.84.130:1701/primo_library/libweb/action/' + book_href[0]
+    if len(book_isbn) == 0:
+        book_isbn.append('')
+    q1.put(location_href)
+    q2.put(book_isbn[0].replace(' ',''))
+    q2.put(book_author[0].replace(' ',''))
+    q2.put(book_publisher[0].replace(' ',''))
+    q2.put(book_name[0])
     
-def analyse_location(page):
+def analyse_location():
+    url = q1.get()
+    response = urllib.urlopen(url)
+    page = response.read()
     doc = etree.HTML(page.decode('utf-8','ignore'))
-    book_ztfh = doc.xpath(u"//cite/text()")
-    book_location = doc.xpath(u"//span[@class='EXLLocationsTitleContainer']/text()")
-    book_state = doc.xpath(u"//td[@class='EXLLocationTableColumn3']/text()")
-    return (book_ztfh[0].encode('utf-8'), book_location[0].encode('utf-8'), book_state[0].encode('utf-8'))
-def main():
-    print analyse(spider(100000)).encode('utf-8')
+    book_ztfh = xpathNull(doc,u"//cite/text()")
+    book_location = xpathNull(doc,u"//span[@class='EXLLocationsTitleContainer']/text()")
+    book_state = xpathNull(doc,u"//td[@class='EXLLocationTableColumn3']/text()")
+    q2.put(book_location[0].strip())
+    q2.put(book_state[0].replace(' ',''))
+    q2.put(book_ztfh[0].replace(' ',''))
 
+def run():
+    analyse_index(300000)
+    if q1.empty() == False:
+        analyse_location()
+    if q2.full() == True:
+        for i in range(7):
+            print q2.get()
 
-main()
+run()
